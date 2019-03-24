63143c9c78623f6faedd2dbea80cee99a0950179
AZS-99 <saher.zachary.adam@icloud.com>
2019-03-24 16:45:14 -0400

    Initialising

--- HelloWorld/server.js
+++
@@ -1,9 +0,0 @@
-const HTTP_PORT = process.env.PORT || 8080; //If there is no website, use 8080
-const express = require("express"); //Include express class 
-const app = express(); //express() is the constructor of express
-
-app.get("/", (req, res) => { //request and respond to lambda 
-    res.send("Hello, Adam!");
-});
-
-app.listen(HTTP_PORT); //link your app to the http
